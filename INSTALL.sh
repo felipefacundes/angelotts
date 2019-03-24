7fe35cdd6d447a1938eee45894cc26f25aa27bf3
felipefacundes <felipe.facundes@gmail.com>
2019-03-24 19:04:56 -0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,4 +4,4 @@ sudo cp speechd.conf /etc/speech-dispatcher/
 sudo pacman -S espeak-ng speech-dispatcher orca onboard
 sudo pacman -U mbrola-3.0.1h-5-x86_64.pkg.tar.xz
 sudo pacman -U mbrola-voices-br1-971105-5-any.pkg.tar.xz
-speak-ng -v mb/mb-br1 -s 124 -a 200 "instalação do Angelo TTS, concluída"
+speak-ng -v mb/mb-br1 -s 124 -a 200 "instalação do Angelo TTS, concluída com sucesso."
